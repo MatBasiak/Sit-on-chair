c80e42db4ca999e0e9be74fdd74a5a43ac09b862
mateusz <mateusz.basiak@gmail.com>
2018-11-08 19:09:35 +0100

    made arrows

--- src/components/mainContent/news/news.js
+++ src/components/mainContent/news/news.js
@@ -6,7 +6,7 @@ const News = () => {
   return (
     <div className={style.container}>
       <div className={style.container_left}>     
-        <div className={style.leftArrow}>left arrow</div>
+        <div className={style.leftArrow}></div>
         <div className={style.image}/>
       </div>
       
@@ -15,7 +15,7 @@ const News = () => {
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo odio, scelerisque ut tellus nec, porttitor sollicitudin sapien. Donec rutrum, nisi at vehicula gravida, orci ex scelerisque diam, vitae pulvinar arcu ipsum et orci</p>
 
               <Button class={style.button} />
-        <div className={style.rightArrow}>right arrow</div>
+        <div className={style.rightArrow}></div>
       </div>
     </div>
   )
